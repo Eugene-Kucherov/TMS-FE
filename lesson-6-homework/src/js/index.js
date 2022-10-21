0914b8af7a05640fb3abed2a42358e3aa9ca9e5a
Eugene-Kucherov <eugku14@gmail.com>
2022-10-21 22:42:21 +0300

    lesson-11-homework

--- lesson-6-homework/src/js/index.js
+++ lesson-6-homework/src/js/index.js
@@ -1,9 +1,31 @@
 const swiper = new Swiper(".swiper", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 20,
   loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    769: {
+      slidesPerView: 3,
+      spaceBetween: 14,
+    },
+    425: {
+      slidesPerView: 2,
+      spaceBetween: 14,
+    },
+  },
+});
+
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
 });

c6380053e20c86025b3b99666c8e4b9d78939fa5
Sergey Kilochytsky <kila.sergey1@gmail.com>
2022-11-04 16:41:09 +0300

    added script to show mobile navigation

--- final-project-webpack/src/js/demo.js
+++ final-project-webpack/src/js/demo.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const burgerButton = document.querySelector(".menu-btn");
+const mobileNavigation = document.querySelector(".navigation-mobile");
+
+burgerButton.addEventListener("click", () => {
+  if (mobileNavigation.classList.contains("opened")) {
+    mobileNavigation.classList.remove("opened");
+  } else {
+    mobileNavigation.classList.add("opened");
+  }
+});
